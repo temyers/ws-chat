64d15d7bb6e4e4e4d0315d73cf641ac910f949b9
Ubuntu <ubuntu@ip-10-3-0-76.ec2.internal>
2016-04-22 07:36:16 +0000

    Update for investigations using SSL Passthrough

--- configure-proxy-protocol.sh
+++ configure-proxy-protocol.sh
@@ -2,8 +2,15 @@
 
 # http://www.raweng.com/blog/2014/11/11/websockets-on-aws-elb/
 
-ELB_NAME = websocket-test
+export ELB_NAME="test-elb-name"
+REGION=us-east-1
 
-aws elb create-load-balancer-policy --load-balancer-name $ELB_NAME --policy-name $ELB_NAME-proxy-protocol –policy-type-name ProxyProtocolPolicyType --policy-attributes AttributeName=ProxyProtocol,AttributeValue=True
+export POLICY_NAME="policy-protocol-name"
 
-aws elb set-load-balancer-policies-for-backend-server --load-balancer-name $ELB_NAME --instance-port 80 --policy-names $ELB_NAME-proxy-protocol
+#aws elb create-load-balancer-policy --load-balancer-name "vgw-chumb-haproxyE-57FBFX2IQL5V" --policy-name "$POLICY_NAME" --policy-type-name "ProxyProtocolPolicyType" --policy-attributes "AttributeName=ProxyProtocol,AttributeValue=True" 
+
+aws elb set-load-balancer-policies-for-backend-server --load-balancer-name $ELB_NAME --instance-port 80 --policy-names $POLICY_NAME
+aws elb set-load-balancer-policies-for-backend-server --load-balancer-name $ELB_NAME --instance-port 8080 --policy-names $POLICY_NAME
+
+# Required for SSL
+aws elb set-load-balancer-policies-for-backend-server --load-balancer-name $ELB_NAME --instance-port 443 --policy-names $POLICY_NAME
